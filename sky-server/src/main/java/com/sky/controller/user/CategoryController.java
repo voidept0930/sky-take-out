ac3053c0c95577092df761749e0c2074d725558c
voidept <voidept0930@gmail.com>
2025-08-26 03:40:52 +0800

    C端分类接口

---
+++ sky-server/src/main/java/com/sky/controller/user/CategoryController.java
@@ -0,0 +1,31 @@
+package com.sky.controller.user;
+
+import com.sky.entity.Category;
+import com.sky.result.Result;
+import com.sky.service.CategoryService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController("userCategoryController")
+@Slf4j
+@RequestMapping("/user/category")
+public class CategoryController {
+
+    private final CategoryService categoryService;
+    @Autowired
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping("/list")
+    public Result<List<Category>> getByType(Integer type){
+        log.info("根据分类类型{}查询分类", type);
+        return Result.success(categoryService.list(type));
+    }
+
+}
